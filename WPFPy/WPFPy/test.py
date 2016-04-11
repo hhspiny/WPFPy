2582b8414680e0d9fd3bd7580169e1e05cee5480
hhspiny <hhspiny@pine.cc>
2016-04-10 22:30:40 -0400

    2nd level inheritence is wrong

--- WPFPy/WPFPy/test.py
+++ WPFPy/WPFPy/test.py
@@ -1,5 +1,6 @@
 import clr, System
 class DotNetINotifyPropertyChanged(System.ComponentModel.INotifyPropertyChanged):
+#class DotNetINotifyPropertyChanged(System.Object):
     __namespace__ = "DotNetINotifyPropertyChanged"
     def __init__(self):
         super(DotNetINotifyPropertyChanged, self).__init__()    
@@ -7,15 +8,52 @@ class DotNetINotifyPropertyChanged(System.ComponentModel.INotifyPropertyChanged)
         pass
     def remove_PropertyChanged(self, value):
         pass
-c1=DotNetINotifyPropertyChanged()
-c1.add_PropertyChanged("text")
-print "C1 success"
+#c1=DotNetINotifyPropertyChanged()
+#print c1.add_PropertyChanged
+#print dir(c1.add_PropertyChanged)
+#print c1.add_PropertyChanged.im_func
+#c1.add_PropertyChanged("text")
+#print "C1 success"
 
 class A(DotNetINotifyPropertyChanged):
     __namespace__ = "A"
     def __init__(self):
         super(A,self).__init__()
-a1 = A()
-a1.add_PropertyChanged("text")
-print "a1 success"
+#a1 = A()
+#print a1.add_PropertyChanged
+#print dir(a1.add_PropertyChanged)
+#a1.add_PropertyChanged("text")
+#print "a1 success"
 
+class baseA(System.Object):
+#    __namespace__ = "A"
+    def __init__(self):
+        pass
+    def methodA(self):
+        print "A"
+class baseB(baseA):
+#    __namespace__ = "B"
+    def __init__(self):
+        super(baseB,self).__init__()
+    def methodB(self):
+        print "B"
+
+class baseNA(System.Random):
+    __namespace__ = "System"
+    def __init__(self):
+        super(baseNA,self).__init__()
+#    @clr.clrmethod(System.String,[])
+    def ToString(self):
+        return "string"
+
+class baseNB(baseNA):
+    __namespace__ = "System"
+    def __init__(self):
+        super(baseNB,self).__init__()
+bna = baseNA()
+print bna.ToString()
+print bna._Random__ToString()
+bnb = baseNB()
+print bnb.ToString()
+print bnb._baseNA__ToString()
+print bnb._Random__ToString()
